86f97261131cfe429efa6b99bfaaf5280ca25ae6
streamich <streamich@gmail.com>
2023-07-02 15:37:26 +0200

    feat: 🎸 recursively render <span> nodes

--- src/ColorTokens.tsx
+++ src/ColorTokens.tsx
@@ -1,7 +1,28 @@
 import * as React from 'react';
 import {tokens} from './util';
-import type {TokenNode} from './types';
-import {ColorToken} from './ColorToken';
+import type {Token} from './types';
+
+const {createElement: h} = React;
+
+const astToReact = (ast: Token, code: string, pos: number, prefix: string): [node: React.ReactNode, length: number] => {
+  if (typeof ast === 'number') return [code.slice(pos, pos + ast), ast];
+  const [type, tokens, language] = ast;
+  const children: React.ReactNode[] = [];
+  const length = tokens.length;
+  let nodeTextLength = 0;
+  for (let i = 0; i < length; i++) {
+    const token = tokens[i];
+    const [node, len] = astToReact(token, code, pos + nodeTextLength, prefix);
+    nodeTextLength += len;
+    children.push(node);
+  }
+  const props = {
+    className: prefix + type,
+    'data-lang': language,
+    children,
+  };
+  return [h('span', props), nodeTextLength];
+};
 
 export interface ColorTokensProps {
   code: string;
@@ -9,15 +30,18 @@ export interface ColorTokensProps {
   prefix?: string;
 }
 
-export const ColorTokens: React.FC<ColorTokensProps> = ({ code, lang, prefix }) => {
-  const [ast, setAst] = React.useState<TokenNode | null>(null);
+export const ColorTokens: React.FC<ColorTokensProps> = ({ code, lang, prefix = ''}) => {
+  const [node, setNode] = React.useState<React.ReactNode | null>(null);
 
   React.useEffect(() => {
-    setAst(null);
+    setNode(null);
     let unmounted = false;
     tokens(code, lang)
       .then((ast) => {
-        if (!unmounted) setAst(ast);
+        if (!unmounted) {
+          const [node] = astToReact(ast, code, 0, prefix);
+          setNode(node);
+        }
       })
       .catch((err) => {});
     return () => {
@@ -25,14 +49,5 @@ export const ColorTokens: React.FC<ColorTokensProps> = ({ code, lang, prefix }) 
     };
   }, [code]);
 
-  if (!ast) return <>{code}</>
-
-  return (
-    <ColorToken
-      code={code}
-      pos={0}
-      token={ast}
-      prefix={prefix}
-    />
-  );
+  return node ? node : <>{code}</>;
 };
