ad8ced45fa2bfa323a8de981493547aa30895414
streamich <streamich@gmail.com>
2024-05-27 13:29:08 +0200

    feat: 🎸 add theming support to <ColorTokens>

--- src/ColorTokens.ts
+++ src/ColorTokens.ts
@@ -1,11 +1,20 @@
 import * as React from "react";
+import {useTheme, rule} from 'nano-theme';
+import * as palettes from './palettes';
 import {Markup, MarkupProps} from './Markup';
+import {css} from './style';
 
-const { createElement: h } = React;
+const lightClass = rule(css(palettes.light));
+const darkClass = rule(css(palettes.dark));
 
 export interface ColorTokensProps extends MarkupProps {
 }
 
 export const ColorTokens: React.FC<ColorTokensProps> = (props) => {
-  return h(Markup, props);
+  const theme = useTheme();
+
+  return React.createElement(Markup, {
+    ...props,
+    className: (props.className ?? '') + (theme.isLight ? lightClass : darkClass),
+  });
 };
---
+++ src/palettes.ts
@@ -0,0 +1,13 @@
+import {palette, ColorPalette} from './style';
+
+export const light: ColorPalette = {
+  ...palette,
+  // variable: '#e00e44',
+  // string: '#e00e44',
+  number: '#0a8F3F',
+  boolean: '#411888',
+};
+
+export const dark: ColorPalette = {
+  ...palette,
+};
